19f922da0462155925d68edbc49fd54a2cf5d757
Jonathan Gonzalez <zaicro-blink@hotmail.com>
2021-05-22 21:52:42 -0500

    se agregaron componentes para edición y creacion de receta y se cambiaron event emitter por subjects

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,6 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { RecipeService } from '../services/recipe.service';
 import { Recipe } from './recipe.model';
 
@@ -9,11 +10,16 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.component.css']
 
 })
-export class RecipesComponent implements OnInit {
+export class RecipesComponent implements OnInit, OnDestroy {
 
+  subs:Subscription;
 
   recipe:Recipe;
   constructor(private recipeSrv:RecipeService) { }
+  
+  ngOnDestroy(): void {
+   this.subs.unsubscribe();
+  }
 
   ngOnInit() {
     this.recipeSrv.recipeSelected.subscribe(recipe=>{
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient-model';
 import { ShoppingListService } from '../services/shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy{
 
+  subs:Subscription;
   
   ingredients:Ingredient[]
   constructor(private srvIngredient:ShoppingListService) { 
 
   }
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
 
   ngOnInit() {
     this.ingredients=this.srvIngredient.getIngredient();
-    this.srvIngredient.ingredientChanged.subscribe(i=>this.ingredients=i);
+    this.subs= this.srvIngredient.ingredientChanged.subscribe(i=>this.ingredients=i);
   }
 
  
